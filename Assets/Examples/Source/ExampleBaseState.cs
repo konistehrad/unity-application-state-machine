e2db14f369d712069c366aa5fac1b7e8132d3dc8
Conrad Kreyling <konistehrad@gmail.com>
2017-09-11 19:15:39 -0400

    more compact fade time

--- Assets/Examples/Source/ExampleBaseState.cs
+++ Assets/Examples/Source/ExampleBaseState.cs
@@ -48,31 +48,26 @@ public class ExampleBaseState : AsyncStateBehaviour
 
     protected IEnumerator FadeIn(float fadeTime = 0.5f, bool useScaledTime = true)
     {
-        float elapsed = 0;
-        GetComponent<CanvasGroup>().alpha = 0;
-        while(elapsed < fadeTime)
-        {
-            GetComponent<CanvasGroup>().alpha = fadeInCurve.Evaluate(
-                Mathf.Clamp01(elapsed/fadeTime)
-            );
-            yield return null;
-            elapsed += useScaledTime ? Time.deltaTime : Time.unscaledDeltaTime;
-        }
-        GetComponent<CanvasGroup>().alpha = 1;
+        return RunFade(0, 1, fadeInCurve, fadeTime, useScaledTime);
     }
 
     protected IEnumerator FadeOut(float fadeTime = 0.5f, bool useScaledTime = true)
     {
+        return RunFade(1, 0, fadeOutCurve, fadeTime, useScaledTime);
+    }
+
+    private IEnumerator RunFade(float starting, float ending, AnimationCurve curve, float fadeTime, bool useScaledTime)
+    {
         float elapsed = 0;
-        GetComponent<CanvasGroup>().alpha = 1;
+        GetComponent<CanvasGroup>().alpha = starting;
         while(elapsed < fadeTime)
         {
-            GetComponent<CanvasGroup>().alpha = fadeOutCurve.Evaluate(
+            GetComponent<CanvasGroup>().alpha = curve.Evaluate(
                 Mathf.Clamp01(elapsed/fadeTime)
             );
             yield return null;
             elapsed += useScaledTime ? Time.deltaTime : Time.unscaledDeltaTime;
         }
-        GetComponent<CanvasGroup>().alpha = 0;
+        GetComponent<CanvasGroup>().alpha = ending;
     }
 }
