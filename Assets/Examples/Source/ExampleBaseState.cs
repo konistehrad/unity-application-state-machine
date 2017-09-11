78fa27041822da0eccec75680aa1d27080410d61
Conrad Kreyling <konistehrad@gmail.com>
2017-09-11 16:37:53 -0400

    start work on example code

---
+++ Assets/Examples/Source/ExampleBaseState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PillowTalk.StateMachine;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class ExampleBaseState : AsyncStateBehaviour 
+{
+    [SerializeField] private ExampleStateMachine stateMachine;
+
+    protected override void Awake()
+    {
+        GetComponent<CanvasGroup>().alpha = 0;
+        DisableInteraction();
+        gameObject.SetActive(false);
+    }
+
+    
+}
---
+++ Assets/Examples/Source/ExampleStateMachine.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PillowTalk.StateMachine;
+using UnityEngine;
+
+public class ExampleStateMachine : AsyncStateMachine 
+{
+    [SerializeField] private ExampleStateA stateA;
+	[SerializeField] private ExampleStateB stateB;
+	[SerializeField] private ExampleStateC stateC;
+
+	public ExampleStateA StateA { get { return stateA; } }
+	public ExampleStateB StateB { get { return stateB; } }
+	public ExampleStateC StateC { get { return stateC; } }
+}
